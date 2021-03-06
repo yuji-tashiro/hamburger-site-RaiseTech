7634f7b1c7c525fa54d69ef76a2893355f1e040b
Yuji <yuji.tashiro1997@gmail.com>
2021-03-06 14:07:46 +0900

    更新

--- footer.php
+++ footer.php
@@ -1,7 +1,12 @@
 <footer class="l-footer p-footer">
     <div class="p-footer__inner">
     <nav class="p-footer__inner__nav">
-        <?php 
+
+        <?php wp_nav_menu( array(
+            'theme_location' => 'footer-menu',
+        ) ); ?>
+
+        <!-- <?php 
         $args = array(
         'menu'=>'f-menus',
         'menu_class'=>'menu',
@@ -9,7 +14,7 @@
         'menu_id'=>'',
         );
         wp_nav_menu($args);
-        ?>
+        ?> -->
     </nav>
         <p class="c-copyright">copyright: RaiseTech</p>
     </div>
