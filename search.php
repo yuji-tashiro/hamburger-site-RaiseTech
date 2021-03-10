e825a59b6e4462af0b02d128e2f3fbd212c20115
Yuji <yuji.tashiro1997@gmail.com>
2021-03-11 06:37:08 +0900

    kousinn

--- search.php
+++ search.php
@@ -9,13 +9,13 @@
          <!--検索キーワードがないとき-->
          <?php echo '<p>検索キーワードが未入力です。</p>'; ?>
       <?php else : ?>
-
          <!--/1,記事があるかの確認/-->
          <?php if( have_posts() ) : ?> 
-         <!--/2,表示する投稿データがあれば繰り返し処理開始 3,ループ処理に必要なカウント処理等/-->
+            <!--/2,表示する投稿データがあれば繰り返し処理開始 3,ループ処理に必要なカウント処理等/-->
             <?php while( have_posts() ) : the_post(); ?>
                <!--4.「ここに出力したい処理などを記述」-->
                <div id="post-<?php the_ID(); ?>" <?php post_class('p-menu-unit'); ?>>
+
                   <?php if ( has_post_thumbnail() ) : ?>
                      <?php 
                         $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
@@ -23,6 +23,7 @@
                      <a class="c-thumbnail" style="background-image: url( '<?php echo $url; ?>' );">
                      </a>
                   <?php endif; ?>
+
                   <div class="p-menu-unit__content">
                      <h2 class="c-headline--lv2--menu-unit"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                      <!--/記事の抜粋を表示させる/-->
@@ -31,28 +32,25 @@
                         <!--/記事ページへのリンクを出力/-->
                         <a href="<?php the_permalink(); ?>">詳しく見る</a>
                      </button>
-                  </div>
+                  </div><!--/p-menu-unit__content/-->
+
                </div><!--/p-menu-unit/-->
-               
-               <div class="c-wrap--pagenation">
-                  <div class="p-current-total-pagenumber">
-                     <ul>
-                        <li>page</li>
-                        <li><?php current_total_pagenumber(); ?></li>
-                     </ul>
-                  </div>
-                  <?php if( function_exists("the_pagenation") ) the_pagenation(); ?>
-               </div>
                
             <?php endwhile; ?> <!--/5,繰り返し処理ここまで。投稿データがまだあればwhileに戻る。なければ終了/-->
          <?php else : ?><!--/6.投稿データがなければ/-->
             <p>表示する記事がありません</p><!--/7.ない時の処理/-->
          <?php endif; ?><!--/8.条件 分岐終了/-->
-
       <?php endif; ?>
 
-
-
+      <div class="c-wrap--pagenation">
+         <div class="p-current-total-pagenumber">
+            <ul>
+               <li>page</li>
+               <li><?php current_total_pagenumber(); ?></li>
+            </ul>
+         </div>
+         <?php if( function_exists("the_pagenation") ) the_pagenation(); ?>
+      </div>
    </main>
 </div><!--/c-wrap--headerMain/-->
 
